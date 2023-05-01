7e0995c3894146ba42e152e6eff3b17ecb094b45
p0lluxstar <web-registra@yandex.ru>
2023-05-01 16:29:09 +0300

    feat: 010523/16:29

---
+++ script2.js
@@ -0,0 +1,186 @@
+const outputTextarea = document.getElementById('outputText');
+outputTextarea.focus();
+outputTextarea.value.toLowerCase();
+
+
+let keyboardEnLower = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace', 'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'backslash', 'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'enter', 'shiftl', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'shiftr', 'top', 'ctrl', 'win', 'alt', 'spacebar', 'alt', 'win', 'ctrl', 'left', 'down', 'right', ];
+let keyboardEnUpper = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace', 'tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', 'backslash', 'capslock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'enter', 'shiftl', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'shiftr', 'top', 'ctrl', 'win', 'alt', 'spacebar', 'alt', 'win', 'ctrl', 'left', 'down', 'right', ];
+
+//создание первоначальной клавиатуры
+function creatKeyBoard(arrSymbol){
+    let out = '';
+    for (let i = 0; i < arrSymbol.length; i++){
+        if(i === 14 || i === 28 || i === 41 || i === 54){
+            out += '<div class="line-break"></div>'
+        }
+        out += '<div class="key" data="' + arrSymbol[i] + '">' + arrSymbol[i] + '</div>';
+
+    }
+
+    document.querySelector('.keyboard').innerHTML = out;
+    
+}
+
+creatKeyBoard(keyboardEnLower);
+
+// добавляем классы функциональным кнопкам
+function addClassFnButton(arg){
+
+    let classActive = 'no-active'
+
+    if(!(typeof arg === 'undefined')){
+        classActive = 'active'
+    }
+ 
+    document.querySelectorAll('.key').forEach(function(e){
+        if (e.getAttribute('data') === 'backspace'){
+            e.innerHTML = 'Backspace';
+            e.classList.add('backspace');
+        }
+        if (e.getAttribute('data') === 'tab'){
+            e.innerHTML = 'Tab';
+            e.classList.add('tab');
+        }
+        if (e.getAttribute('data') === 'backslash'){
+            e.innerHTML = '\\';
+            e.classList.add('backslash');
+        }
+        if (e.getAttribute('data') === 'capslock'){
+            e.innerHTML = 'Caps Lock';
+            e.classList.add('capslock', `${classActive}`);
+        }
+        if (e.getAttribute('data') === 'enter'){
+            e.innerHTML = 'Enter';
+            e.classList.add('enter');
+        }
+        if (e.getAttribute('data') === 'shiftl'){
+            e.innerHTML = 'Shift';
+            e.classList.add('shiftl');
+        }
+        if (e.getAttribute('data') === 'shiftr'){
+            e.innerHTML = 'Shift';
+            e.classList.add('shiftr');
+        }
+        if (e.getAttribute('data') === 'ctrl'){
+            e.innerHTML = 'Ctrl';
+            e.classList.add('ctrl');
+        }
+        if (e.getAttribute('data') === 'win'){
+            e.innerHTML = 'Win';
+            e.classList.add('win');
+        }
+        if (e.getAttribute('data') === 'spacebar'){
+            e.innerHTML = '';
+            e.classList.add('spacebar');
+        }
+        if (e.getAttribute('data') === 'alt'){
+            e.innerHTML = 'Alt';
+            e.classList.add('alt');
+        }
+        if (e.getAttribute('data') === 'top'){
+            e.innerHTML = '↑';
+            e.classList.add('top');
+        }
+        if (e.getAttribute('data') === 'left'){
+            e.innerHTML = '←';
+            e.classList.add('left');
+        }
+        if (e.getAttribute('data') === 'down'){
+            e.innerHTML = '↓';
+            e.classList.add('down');
+        }
+        if (e.getAttribute('data') === 'right'){
+            e.innerHTML = '→';
+            e.classList.add('right');
+        }
+    })
+}
+
+addClassFnButton();
+
+//отвечает за вывод символов в textarea
+function addSybmoInTextarea(){
+    document.querySelectorAll('.keyboard .key').forEach(function(e){
+        e.addEventListener('click', () => {
+            if(e.innerHTML === 'Caps Lock' || e.innerHTML === 'Backspace' || e.innerHTML === 'Enter'){
+               return;
+            } else {
+                outputTextarea.value += e.innerHTML; 
+            }
+        }) 
+    });
+    
+}
+
+addSybmoInTextarea();
+
+/* caps lock */
+// отвечате за работу при клике мышкой
+function workCapsLockClick(){ 
+
+    const capsLock = document.querySelector('.capslock');
+    capsLock.addEventListener('click', () => {
+
+        if(!capsLock.classList.contains('active')){
+            creatKeyBoard(keyboardEnUpper);
+            addClassFnButton('active');
+            addSybmoInTextarea();
+            workCapsLockClick();
+            workCapsLockPress()
+        }else {
+            creatKeyBoard(keyboardEnLower);
+            addClassFnButton();
+            addSybmoInTextarea();
+            workCapsLockClick();
+            workCapsLockPress()
+        }
+        
+    });
+}
+workCapsLockClick();
+
+function workCapsLockPress(){
+    const capsLock = document.querySelector('.capslock');
+
+    document.addEventListener('keydown', function(e){
+        outputTextarea.focus();
+
+       if (e.key === 'CapsLock'){
+            if(capsLock.classList.contains('no-active')){
+                capsLock.classList.add('active')
+                capsLock.classList.remove('no-active')
+                creatKeyBoard(keyboardEnUpper);
+                addClassFnButton('active');
+                addSybmoInTextarea();
+                workCapsLockClick();
+                workCapsLockPress()
+            }else{
+                capsLock.classList.remove('active')
+                capsLock.classList.add('no-active')
+                creatKeyBoard(keyboardEnLower);
+                addClassFnButton();
+                addSybmoInTextarea();
+                workCapsLockClick();
+                workCapsLockPress()
+            }
+       }
+
+        document.querySelectorAll('.keyboard .key').forEach(el =>{
+            if(e.key === el.innerHTML){
+                el.classList.add('active') 
+                
+            }
+        })
+
+        function delClassActive(){
+            document.querySelectorAll('.keyboard .key').forEach(el =>{
+                if(!(el.innerHTML === 'Caps Lock')){
+                    el.classList.remove('active');
+                } 
+            })
+        }
+        setTimeout(delClassActive, 120)
+    })
+}
+
+workCapsLockPress()
--- script3.js
+++ script3.js
@@ -1,133 +1,4 @@
-const outputTextarea = document.getElementById('outputText');
-outputTextarea.focus();
 
-let keyboardEn = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 'backspace', 'tab', 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 'backslash', 'capslock', 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 'enter', 'shiftl', 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 'shiftr', 'top', 'ctrl', 'win', 'alt', 'spacebar', 'alt', 'win', 'ctrl', 'left', 'down', 'right', ];
-
-//создание первоначальной клавиатуры
-function creatKeyBoard(){
-    let out = '';
-    for (let i = 0; i < keyboardEn.length; i++){
-        if(i === 14 || i === 28 || i === 41 || i === 54){
-            out += '<div class="line-break"></div>'
-        }
-        out += '<div class="key" data="' + keyboardEn[i] + '">' + String.fromCharCode(keyboardEn[i]) + '</div>';
-
-    }
-
-    document.querySelector('.keyboard').innerHTML = out;
-    
-}
-
-creatKeyBoard();
-
-// добавляем классы функциональным кнопкам
-function addClassFnButton(arg){
-    let classActive = 'no-active'
-
-    if(!(typeof arg === 'undefined')){
-        classActive = 'active'
-    }
-
-    document.querySelectorAll('.key').forEach(function(e){
-        if (e.getAttribute('data') === 'backspace'){
-            e.innerHTML = 'Backspace';
-            e.classList.add('backspace');
-        }
-        if (e.getAttribute('data') === 'tab'){
-            e.innerHTML = 'Tab';
-            e.classList.add('tab');
-        }
-        if (e.getAttribute('data') === 'backslash'){
-            e.innerHTML = '\\';
-            e.classList.add('backslash');
-        }
-        if (e.getAttribute('data') === 'capslock'){
-            e.innerHTML = 'Caps Lock';
-            e.classList.add('capslock', `${classActive}`);
-        }
-        if (e.getAttribute('data') === 'enter'){
-            e.innerHTML = 'Enter';
-            e.classList.add('enter');
-        }
-        if (e.getAttribute('data') === 'shiftl'){
-            e.innerHTML = 'Shift';
-            e.classList.add('shiftl');
-        }
-        if (e.getAttribute('data') === 'shiftr'){
-            e.innerHTML = 'Shift';
-            e.classList.add('shiftr');
-        }
-        if (e.getAttribute('data') === 'ctrl'){
-            e.innerHTML = 'Ctrl';
-            e.classList.add('ctrl');
-        }
-        if (e.getAttribute('data') === 'win'){
-            e.innerHTML = 'Win';
-            e.classList.add('win');
-        }
-        if (e.getAttribute('data') === 'spacebar'){
-            e.innerHTML = '';
-            e.classList.add('spacebar');
-        }
-        if (e.getAttribute('data') === 'alt'){
-            e.innerHTML = 'Alt';
-            e.classList.add('alt');
-        }
-        if (e.getAttribute('data') === 'top'){
-            e.innerHTML = '↑';
-            e.classList.add('top');
-        }
-        if (e.getAttribute('data') === 'left'){
-            e.innerHTML = '←';
-            e.classList.add('left');
-        }
-        if (e.getAttribute('data') === 'down'){
-            e.innerHTML = '↓';
-            e.classList.add('down');
-        }
-        if (e.getAttribute('data') === 'right'){
-            e.innerHTML = '→';
-            e.classList.add('right');
-        }
-    })
-}
-
-addClassFnButton();
-
-//отвечает за вывод символов в textarea
-function addSybmoInTextarea(){
-    document.querySelectorAll('.keyboard .key').forEach(function(e){
-        e.addEventListener('click', () => {
-            if(e.innerHTML === 'Caps Lock' || e.innerHTML === 'Backspace' || e.innerHTML === 'Enter'){
-               return;
-            } else {
-                outputTextarea.value += e.innerHTML; 
-            }
-        }) 
-    });
-    
-}
-
-addSybmoInTextarea();
-
-//при нажатии на клавиши создается эффект нажатия
-function showEffectPressClick(){
-    document.onkeypress = function (e){
-        outputTextarea.focus();
-    
-        function delClassActive(){
-            document.querySelectorAll('.keyboard .key').forEach(function(e){
-                if(!(e.innerHTML === 'Caps Lock')){
-                    e.classList.remove('active');
-                }
-            })
-        }
-        setTimeout(delClassActive, 100);
-    
-        document.querySelector('.keyboard .key[data="' + e.keyCode + '"]').classList.add('active')
-    }
-}
-showEffectPressClick();
 
 /* document.onkeypress = function(e){
    
@@ -144,7 +15,7 @@ function globalWorkCapsLock(){
     if(!capsLock.classList.contains('active')){
         console.log('active no')
        
-        creatKeyBoard();
+       
         addClassFnButton();
         addSybmoInTextarea();
         workCapsLockClick();
@@ -186,7 +57,7 @@ function workCapsLockClick(){
         }else {
             console.log(2)
             capsLock.classList.remove('active');
-            creatKeyBoard();
+            
             addClassFnButton();
             addSybmoInTextarea();
             workCapsLockClick();
@@ -295,11 +166,38 @@ function autoDelClassActive(nameClass){
     setTimeout(delClassActive, 100);
 }
 
+//при нажатии на клавиши создается эффект нажатия
+function showEffectPressClick(){
+    document.onkeypress = function (e){
+        outputTextarea.focus();
+    
+        function delClassActive(){
+            document.querySelectorAll('.keyboard .key').forEach(function(e){
+                if(!(e.innerHTML === 'Caps Lock')){
+                    e.classList.remove('active');
+                }
+            })
+        }
+        setTimeout(delClassActive, 100);
+    
+        document.querySelector('.keyboard .key[data="' + e.keyCode + '"]').classList.add('active')
+    }
+}
+showEffectPressClick();
+
 //
+let arr = []
 document.addEventListener('keydown', function(e) {
+   
+    arr.push(e.key)
+    console.log(arr)
+
     e.preventDefault();
     console.log(e)
     if(e.code === 'CapsLock'){
+       
+        creatKeyBoard(keyboardEn3)
+        addClassFnButton();
         document.querySelector('.capslock').classList.toggle('active');
     }
     if(e.code === 'Space'){
